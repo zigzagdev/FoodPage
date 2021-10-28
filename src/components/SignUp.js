1baebd15d6d1110e41c67de4c649da80a179f25d
Masatoshi <msts.oo0131@gmail.com>
2021-10-29 00:56:33 +0900

    label_input_change

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,13 +1,27 @@
 import React from "react";
 
 const style = {
+  marginTop:40,
+  marginLeft: 70,
+}
+
+const style2 = {
   color:"Blue",
-  marginTop:30,
-  marginLeft: 20,
+  fontSize: 27,
+  display:'block',
 }
 
 const SignUp = () => {
-    return <h1 style={style}>Author Register</h1>;
-}
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('register');
+    }
+    return (
+        <div style={style}>
+          <label style={style2}>管理者メールアドレス</label>
+          <input name="email" type="email" size='29'  placeholder="ex.)test@com"  style={{marginTop:15,marginLeft:21}}/>
+        </div>
+    )
+};
 
 export default SignUp;
