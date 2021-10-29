350bd6a1b86083c106feaa117e02922e88cfc6e3
Masatoshi <msts.oo0131@gmail.com>
2021-10-29 11:28:20 +0900

    Fragment_Header_add

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,41 @@
-import React from "react";
+import React,{Fragment} from "react";
 
-const style = {
+const pstyle = {
+ display:'flex',
+ justifyContent:'space-between',
  height: 100,
  backgroundColor: '#0033FF',
  textAlign: "right",
  minWidth:'1024px',
+}
+
+const style = {
+ textAlign: "right",
  paddingRight: 100,
  lineHeight:3,
  fontSize: 30
 }
 
+const style2 = {
+  textAlign: "left",
+  fontSize: 30,
+  paddingLeft: 100,
+  lineHeight:3,
+
+}
+
+
 const Header = () => {
-   return <div style={style}>
-            <label style={{color:"white"}}>Food Search</label>
-          </div>;
+   return<Fragment>
+           <div style={pstyle}>
+             <div style={style2}>
+               <label style={{color:"white"}}>Food Search</label>
+             </div>
+               <div style={style}>
+                   <label style={{color:"white"}}>Test</label>
+               </div>
+           </div>
+         </Fragment>;
 }
 
 export default Header;
