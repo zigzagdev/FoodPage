926cb9000a0296ee67b06dfa3da292c465b934ce
Masatoshi <msts.oo0131@gmail.com>
2021-11-03 16:20:03 +0900

    SignUp_Page_login_mode

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {useState} from "react";
 import {auth} from  '../firebase';
+import {useAuthContext} from "./authcontext";
 
 
 const style = {
@@ -15,35 +16,31 @@ const style2 = {
 }
 
 const SignUp = () => {
-    const [email,setEmail] = useState('');
-    const [password,setPassword] = useState('');
+    const {user} = useAuthContext();
     const handleSubmit = (e) => {
         e.preventDefault();
         const{email,password} = e.target.elements;
         auth.createUserWithEmailAndPassword(email.value,password.value)
     };
-    const handleChangeEmail = (e) => {
-        setEmail(e.currentTarget.value);
-    }
-    const handleChangePassword = (e) => {
-        setPassword(e.currentTarget.value);
-    };
 
     return (
-      <form onSubmit={handleSubmit}>
-        <div style={style}>
-          <label style={style2}>管理者メールアドレス</label>
-          <input name="email" type="email" size='29'  placeholder="ex.)test@com"  style={{marginTop:15,marginLeft:7}} onChange={(e) => handleChangeEmail(e)}/>
-        </div>
-        <div style={style}>
-          <label style={style2}>管理者パスワード</label>
-          <input name="password" type="password" size='29' style={{marginTop:15,marginLeft:7}} onChange={(e) => handleChangePassword(e)}/>
-          <label style={{fontSize:15,color:'#FF77FF'}}>※英数字8文字以上でお願いします。</label>
-        </div>
-        <div>
-          <button style={{color:"#222222",marginLeft:76,marginTop:29}}>Register</button>
-        </div>
-      </form>
+    <div>
+      <h1 style={{color:'#110000',marginLeft:70,}}>Admin登録</h1>
+        <form onSubmit={handleSubmit}>
+          <div style={style}>
+            <label style={style2}>管理者メールアドレス</label>
+            <input name="email" type="email" size='29'  placeholder="ex.)test@com"  style={{Top:15,marginLeft:7}}/>
+          </div>
+          <div style={style}>
+            <label style={style2}>管理者パスワード</label>
+            <input name="password" type="password" size='29' style={{marginTop:15,marginLeft:7}}/>
+            <label style={{fontSize:15,color:'#FF77FF'}}>    ※英数字8文字以上でお願いします。</label>
+          </div>
+          <div>
+            <button style={{color:"#222222",marginLeft:76,marginTop:29}}>Register</button>
+          </div>
+        </form>
+    </div>
     );
 };
 
