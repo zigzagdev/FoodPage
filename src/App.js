b51d2c14c0bb3a61fbe08cc6290e131fd65e4ced
Masatoshi <msts.oo0131@gmail.com>
2021-11-13 12:06:17 +0900

    Footer_create

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import SignUp from "./components/SignUp";
 import Header from "./components/Header";
 import {AuthProvider} from "./components/authcontext";
 import {Route,BrowserRouter} from "react-router-dom";
+import Footer from "./components/Footer";
 
 
 function App() {
